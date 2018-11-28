39de6a5e18ed59fdb6822be7cb1c299a28dda72e
RRiber <44023499+RRiber@users.noreply.github.com>
2018-11-28 11:06:11 +0100

    tændstik

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Random ændring");
+
+        RibersShoutOut gangster = new RibersShoutOut(10);
+
+        
+
     }
 }

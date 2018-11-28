09d96a365479652e15a7f9c8cb64b42627d5426a
Fsonberg <44023486+Fsonberg@users.noreply.github.com>
2018-11-28 11:03:13 +0100

    asd

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Random ændring");
+        System.out.println("New Branch random ændring");
     }
 }

ed38601e81a8c15a403abc087bcf6397f6001823
Fsonberg <44023486+Fsonberg@users.noreply.github.com>
2018-11-28 11:13:47 +0100

    aijsdla

---
+++ src/Fisk.java
@@ -0,0 +1,12 @@
+public class Fisk {
+    public void h() {
+
+        for (int i = 0; i < 20 ; i++) {
+            for (int j = 0; j < 20 ; j++) {
+                System.out.println("HEJ");
+
+            }
+
+        }
+    }
+}
